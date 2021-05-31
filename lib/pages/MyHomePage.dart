d9976b799ec4652de9f55d13042452910873b807
RahmaYasser <rahma.y9079@gmail.com>
2021-05-31 22:13:51 +0200

    text button and padding

--- lib/pages/MyHomePage.dart
+++ lib/pages/MyHomePage.dart
@@ -9,20 +9,35 @@ class _HomePageState extends State<MyHomePage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-          children: [
-            Expanded(
-                flex: 1,
-                child: Container(color: Colors.black12,),),
-
-            Expanded(
-                flex: 2,
-              child: Container(color: Colors.black,),),
-            Expanded(
-                flex: 1,
-              child: Container(color: Colors.black12,),),
-          ],
-      ),
+      body:Container(
+        color: Colors.white70,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child:Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blueGrey.shade900,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Image(image: AssetImage('logo.png',)),
+                      Spacer(),
+                      TextButton(onPressed: (){}, child: Text("About", style: TextStyle(color: Colors.white),)),
+                      TextButton(onPressed: (){}, child: Text("Categories", style: TextStyle(color: Colors.white),)),
+                      TextButton(onPressed: (){}, child: Text("Search", style: TextStyle(color: Colors.white),))
+                    ],
+                  ),
+                ),
+              ),
+              Row(),
+              Row(),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
